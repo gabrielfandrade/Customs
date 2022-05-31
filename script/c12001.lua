c448ec13a53af9e0035e9833dc0e6c55a413ffe9
Gabriel Faustino de Andrade <30814461+gabrielfandrade@users.noreply.github.com>
2022-05-31 11:28:51 -0300

    Delete script directory

--- script/c12001.lua
+++
@@ -1,6 +0,0 @@
---Spare Noble Arms - Dannel
-local s,id=GetID()
-function s.initial_effect(c)
-	aux.AddEquipProcedure(c,nil,aux.FilterBoolFunction(Card.IsCode,TOKEN_BRAVE))
-end
-s.listed_names={TOKEN_BRAVE}
