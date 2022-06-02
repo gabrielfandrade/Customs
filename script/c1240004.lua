ba509de4541caa1b0c26c517b929e08e06c11eed
Biel <gabrielfa.andrade@gmail.com>
2022-06-02 18:15:11 -0300

    Script - Barinade, Bio-Electric Anemone

---
+++ script/c1240004.lua
@@ -0,0 +1,83 @@
+--Barinade, Bio-Electric Anemone
+local s,id=GetID()
+function s.initial_effect(c)
+	--Cannot be Special Summoned.
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	c:RegisterEffect(e1)
+	--Tribute requires
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_TRIBUTE_LIMIT)
+	e2:SetValue(s.tlimit)
+	c:RegisterEffect(e2)
+	--Summon with 3 Tribute
+	local e3=aux.AddNormalSummonProcedure(c,true,false,3,3,SUMMON_TYPE_TRIBUTE,aux.Stringid(id,0))
+	local e4=aux.AddNormalSetProcedure(c)
+	--Summon cannot be negated
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetCode(EFFECT_CANNOT_DISABLE_SUMMON)
+	e5:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	c:RegisterEffect(e5)
+	--negate
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(id,0))
+	e6:SetCategory(CATEGORY_DISABLE+CATEGORY_DAMAGE)
+	e6:SetType(EFFECT_TYPE_IGNITION)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetCountLimit(1)
+	e6:SetCondition(s.discon)
+	e6:SetTarget(s.distg)
+	e6:SetOperation(s.disop)
+	c:RegisterEffect(e6)
+	--Set LP to 4000 if is destroyed on the field
+	local e7=Effect.CreateEffect(c)
+	e7:SetDescription(aux.Stringid(id,1))
+	e7:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e7:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e7:SetCode(EVENT_DESTROYED)
+	e7:SetCondition(s.lpcon)
+	e7:SetOperation(s.lpop)
+	c:RegisterEffect(e7)
+end
+function s.tlimit(e,c)
+	return not c:IsRace(RACE_AQUA+RACE_FISH)
+end
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsDefensePos()
+end
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+	local ct=Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,ct*200)
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+	local tc=g:GetFirst()
+	local c=e:GetHandler()
+	for tc in aux.Next(g) do
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e2)
+	end
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	local ct=Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)
+	Duel.Damage(p,ct*200,REASON_EFFECT)
+end
+function s.lpcon(e,tp,eg,ep,ev,re,r,rp)
+	return (r&REASON_EFFECT+REASON_BATTLE)~=0 and e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD) and Duel.GetLP(tp)<4000
+end
+function s.lpop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SetLP(tp,4000)
+end
