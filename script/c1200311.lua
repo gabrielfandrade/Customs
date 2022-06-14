b196822324ac43ec78016d44522fd376a4889d17
Biel <gabrielfa.andrade@gmail.com>
2022-06-14 20:18:08 -0300

    Script - First Mechanical Discovery

---
+++ script/c1200311.lua
@@ -0,0 +1,101 @@
+--First Mechanical Discovery
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--decrease tribute
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,2))
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_SUMMON_PROC)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_HAND,0)
+	e2:SetCountLimit(1,{id,0})
+	e2:SetCondition(s.ntcon)
+	e2:SetTarget(aux.FieldSummonProcTg(s.nttg))
+	c:RegisterEffect(e2)
+	--Normal Summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_SUMMON)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetCountLimit(1)
+	e3:SetCondition(s.sumcon)
+	e3:SetTarget(s.sumtg)
+	e3:SetOperation(s.sumop)
+	c:RegisterEffect(e3)
+	--Place 2 "Mechanical" Pendulums in your Pendulum Zones
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_FZONE)
+	e4:SetCountLimit(1,{id,1})
+	e4:SetCost(s.plcost)
+	e4:SetTarget(s.pltg)
+	e4:SetOperation(s.plop)
+	c:RegisterEffect(e4)
+end
+
+function s.ntcon(e,c,minc)
+	if c==nil then return true end
+	return minc==0 and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+end
+function s.nttg(e,c)
+	return c:IsLevel(10)
+end
+
+function s.sumcfilter(c,tp)
+	return c:IsSummonType(SUMMON_TYPE_XYZ) and c:IsSetCard(0x1230) and c:IsSummonPlayer(tp)
+end
+function s.sumcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.sumcfilter,1,nil,tp)
+end
+function s.filter(c)
+	return c:IsLevel(10) and c:IsSummonable(true,nil)
+end
+function s.sumtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_SUMMON,nil,1,0,0)
+end
+function s.sumop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil)
+	if #g>0 then
+		Duel.Summon(tp,g:GetFirst(),true,nil)
+	end
+end
+
+function s.plcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST)
+end
+function s.pcfilter(c)
+	return c:IsSetCard(0x1230) and c:IsType(TYPE_PENDULUM) and not c:IsForbidden()
+end
+function s.pltg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local g=Duel.GetMatchingGroup(s.pcfilter,tp,LOCATION_DECK,0,nil)
+		return Duel.CheckLocation(tp,LOCATION_PZONE,0) and Duel.CheckLocation(tp,LOCATION_PZONE,1)
+	end
+end
+function s.plop(e,tp,eg,ep,ev,re,r,rp)
+	--Place 2 "Mechanical" monsters in your Pendulum Zones
+	if Duel.CheckLocation(tp,LOCATION_PZONE,0) and Duel.CheckLocation(tp,LOCATION_PZONE,1) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+		local pg=Duel.SelectMatchingCard(tp,s.pcfilter,tp,LOCATION_DECK,0,2,2,nil)
+		if #pg~=2 then return end
+		local pc1,pc2=pg:GetFirst(),pg:GetNext()
+		if Duel.MoveToField(pc1,tp,tp,LOCATION_PZONE,POS_FACEUP,false) then
+			if Duel.MoveToField(pc2,tp,tp,LOCATION_PZONE,POS_FACEUP,false) then
+				pc2:SetStatus(STATUS_EFFECT_ENABLED,true)
+			end
+			pc1:SetStatus(STATUS_EFFECT_ENABLED,true)
+		end
+	end
+end
