838aa901d893b177395bd1e73176e39d77fc3f01
Biel <gabrielfa.andrade@gmail.com>
2022-12-28 11:25:22 -0300

    Script - Lightos' Oracles

---
+++ script/c219.lua
@@ -0,0 +1,48 @@
+--Lightos' Oracles
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special summon 1 token to your field
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	e1:SetHintTiming(0,TIMING_END_PHASE)
+	c:RegisterEffect(e1)
+end
+
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,1000) end
+	Duel.PayLPCost(tp,1000)
+end
+
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0x7d0,TYPES_TOKEN+TYPE_TUNER,1000,1000,4,RACE_BEAST,ATTRIBUTE_LIGHT) end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
+end
+function s.spfilter(c,e,tp,atk)
+	return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsAttackBelow(atk) 
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,id+1,0x7d0,TYPES_TOKEN+TYPE_TUNER,1000,1000,4,RACE_BEAST,ATTRIBUTE_LIGHT) then
+		local token=Duel.CreateToken(tp,id+1)
+		Duel.SpecialSummon(token,0,tp,tp,false,false,POS_FACEUP)
+	end
+	local atk=math.abs(Duel.GetLP(tp)-Duel.GetLP(1-tp))
+	if Duel.GetLP(tp)>Duel.GetLP(1-tp)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp,atk)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,e,tp,atk)
+			if #sg<1 then return end
+			Duel.BreakEffect()
+			Duel.SpecialSummon(sg,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
