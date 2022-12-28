ded5812481b7812798c01406dc2184952cbdc721
Biel <gabrielfa.andrade@gmail.com>
2022-12-28 11:19:25 -0300

    Script - Lightos' Blessing

--- script/c218.lua
+++ script/c218.lua
@@ -1,33 +1,35 @@
---Lightos' Call
+--Lightos' Blessing
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetCategory(CATEGORY_RECOVER)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-s.listed_series={0x7d0}
-function s.filter(c)
-	return c:IsSetCard(0x7d0) and c:IsMonster() and c:IsAbleToHand()
+
+function s.recfilter(c)
+	return c:IsAttribute(ATTRIBUTE_LIGHT) and c:GetAttack()>0
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.recfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.recfilter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,s.recfilter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,g:GetFirst():GetAttack())
 end
 function s.spfilter(c,e,tp,atk)
 	return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsAttackBelow(atk) 
 		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
-	if #g>0 and Duel.SendtoHand(g,nil,REASON_EFFECT)~=0 then
-		Duel.ConfirmCards(1-tp,g)
-		Duel.Damage(tp,g:GetFirst():GetAttack(),REASON_EFFECT)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() and tc:GetAttack()>0 then
+		Duel.Recover(tp,tc:GetAttack(),REASON_EFFECT)
 	end
 	local atk=math.abs(Duel.GetLP(tp)-Duel.GetLP(1-tp))
 	if Duel.GetLP(tp)>Duel.GetLP(1-tp)
