bd6397987b33a98a8cec534b632a3a462895db1c
Gabriel Faustino de Andrade <gabrielfa.andrade@gmail.com>
2024-01-28 18:30:26 -0300

    Script - For Tormentas!

--- script/c903.lua
+++ script/c903.lua
@@ -10,6 +10,16 @@ function s.initial_effect(c)
 	e1:SetTarget(s.destg)
 	e1:SetOperation(s.desop)
 	c:RegisterEffect(e1)
+	--Attach 1 Thunder monster from your hand as material
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.mattg)
+	e2:SetOperation(s.matop)
+	c:RegisterEffect(e2)
 end
 
 function s.get_count(g)
@@ -32,7 +42,8 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return ct>1
 end
 function s.desfilter(c,e)
-	return c:IsType({TYPE_MONSTER,TYPE_SPELL+TYPE_TRAP}) and c:IsCanBeEffectTarget() and c:IsCanBeEffectTarget(e)
+	return (c:IsType(TYPE_MONSTER) or c:IsType(TYPE_SPELL+TYPE_TRAP))
+		and c:IsCanBeEffectTarget(e)
 end
 function s.rescon(sg)
 	return #sg==1 or (sg:IsExists(Card.IsType,1,nil,TYPE_MONSTER) and sg:IsExists(Card.IsType,1,nil,TYPE_SPELL+TYPE_TRAP))
@@ -49,5 +60,29 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetTargetCards(e)
 	if #g>0 then
 		Duel.Destroy(g,REASON_EFFECT)
+	end
+end
+
+function s.xyzfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_THUNDER) and c:IsType(TYPE_XYZ)
+end
+function s.matfilter(c)
+	return c:IsRace(RACE_THUNDER)
+end
+function s.mattg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and s.xyzfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.xyzfilter,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.IsExistingMatchingCard(s.matfilter,tp,LOCATION_HAND,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,s.xyzfilter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function s.matop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+		local g=Duel.SelectMatchingCard(tp,s.matfilter,tp,LOCATION_HAND,0,1,1,nil)
+		if #g>0 then
+			Duel.Overlay(tc,g)
+		end
 	end
 end
